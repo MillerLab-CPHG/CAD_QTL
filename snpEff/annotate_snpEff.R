d194b19fb0aa0369e170a87f7676ae496a6c0f9e
chanibravo <48288433+chanibravo@users.noreply.github.com>
2022-11-08 08:44:12 -0500

    Update annotate_snpEff.R

--- snpEff/annotate_snpEff.R
+++ snpEff/annotate_snpEff.R
@@ -20,18 +20,18 @@ b <- as.data.table("gene_id")
 ## Clunky but functional loop to grab the right combo of variant-gene based on your eQTL results
 for (i in seq_along(1:length(transcripts))) {
 trans_test=e[i]
-splits <- as.data.table(strsplit(trans_test[, annotation], "\\|\\|\\|\\|\\|\\|"))
+splits <- as.data.table(strsplit(trans_test[, annotation], "\\|\\|\\|\\|\\|\\|")) ##Split string of results for each SNP with "||||||" separation
 splits[, var_annot:=str_match(splits[,V1], "\\|.+?variant")]
 splits$var_annot <- substr(splits$var_annot, 2, 50) 
 splits[, enst:=str_match(splits[,V1], "ENST0000.+?\\.[0-9]")]
 splits$V1 <- NULL; splits <- splits[enst %like% "ENST"]
 ensts <- as.vector(splits$enst)
-trans_test[, realsnp:=ifelse(transcript %in% ensts, 1, 0)]
+trans_test[, realsnp:=ifelse(transcript %in% ensts, 1, 0)] ##Label SNPs for which the annotation matches your QTL input gene
 trans_test$annotation <- NULL
 setkey(trans_test, transcript); setkey(splits, enst)
 a <- trans_test[splits]
 b <- bind_rows(b, a)
 }
 
-c <- b[realsnp==1, c(2,3,5:11,4,12:14,18)]
+c <- b[realsnp==1, c(2,3,5:11,4,12:14,18)] ##Exclude SNPs which were annotated to a different gene (i.e., not a QTL for that gene)
 write.table(c, "snpeff_annotated_results_date_year.txt", row.names=F, quote=F, sep="\t")
